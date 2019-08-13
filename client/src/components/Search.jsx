d0d352c1d8b2558e989fc28dab0da6f8d947ccf2
MattRodigheri <mrodigheri@gmail.com>
2019-08-13 10:49:55 -0700

    getting first names in suggestions

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,126 +1,18 @@
-// import React from "react";
-// import Autosuggest from "react-autosuggest";
-
-// class Search extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       value: ""
-//     };
-//   }
-
-//   //   getSuggestions(value) {
-//   //     const inputValue = value.trim().toLowerCase();
-//   //     const inputLength = inputValue.length;
-
-//   //     return inputLength === 0
-//   //       ? []
-//   //       : this.props.names.filter(
-//   //           // need to enable more than just first name
-//   //           name => {
-//   //             name.first.toLowerCase().slice(0, inputLength) === inputValue;
-//   //             // console.log(name);
-//   //           }
-//   //         );
-//   //   }
-
-//   onChange(event, { newValue }) {
-//     this.setState({
-//       value: newValue
-//     });
-//   }
-
-//   onSuggestionsFetchRequested({ value }) {
-//     this.setState({
-//       suggestions: getSuggestions(value)
-//     });
-//   }
-
-//   onSuggestionsClearRequested() {
-//     this.setState({
-//       suggestions: []
-//     });
-//   }
-
-//   render() {
-//     const inputProps = {
-//       placeholder: "Type a programming language",
-//       value: this.state.value,
-//       onChange: this.onChange
-//     };
-
-//     return (
-//       <div>
-//         <input
-//           type="text"
-//           //   onChange={event => this.getSuggestions(event.target.value)}
-//         />
-//         <button>Search</button>
-//         <Autosuggest
-//           suggestions={this.props.names}
-//           onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-//           onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-//           getSuggestionValue={getSuggestionValue}
-//           renderSuggestion={renderSuggestion}
-//           inputProps={inputProps}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Search;
-
 import React from "react";
 import Autosuggest from "react-autosuggest";
 
-// Imagine you have a list of languages that you'd like to autosuggest.
-const languages = [
-  {
-    name: "C",
-    year: 1972
-  },
-  {
-    name: "Elm",
-    year: 2012
-  }
-];
-
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
-
-  return inputLength === 0
-    ? []
-    : languages.filter(
-        lang => lang.name.toLowerCase().slice(0, inputLength) === inputValue
-      );
-};
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => <div>{suggestion.name}</div>;
-
 class Search extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
     this.state = {
       value: "",
       suggestions: []
     };
 
+    this.getSuggestions = this.getSuggestions.bind(this);
+    this.getSuggestionValue = this.getSuggestionValue.bind(this);
+    this.renderSuggestion = this.renderSuggestion.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(
       this
@@ -130,6 +22,27 @@ class Search extends React.Component {
     );
   }
 
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    return inputLength === 0
+      ? []
+      : this.props.names.filter(
+          name => name.first.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  }
+
+  // When suggestion is clicked, Autosuggest needs to populate the input
+  // based on the clicked suggestion.
+  getSuggestionValue(suggestion) {
+    return suggestion.first;
+  }
+
+  renderSuggestion(suggestion) {
+    return <div>{suggestion.first}</div>;
+  }
+
   onChange(event, { newValue }) {
     this.setState({
       value: newValue
@@ -137,10 +50,9 @@ class Search extends React.Component {
   }
 
   // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
   onSuggestionsFetchRequested({ value }) {
     this.setState({
-      suggestions: getSuggestions(value)
+      suggestions: this.getSuggestions(value)
     });
   }
 
@@ -161,14 +73,13 @@ class Search extends React.Component {
       onChange: this.onChange
     };
 
-    // Finally, render it!
     return (
       <Autosuggest
         suggestions={suggestions}
         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
         inputProps={inputProps}
       />
     );
