fda477e5109c9c8e42d75cd8b0cda8dae4b20ba8
MattRodigheri <mrodigheri@gmail.com>
2019-08-12 19:20:05 -0700

    names are alphabetized

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tabs, Tab } from "react-bootstrap";
+import sort from "fast-sort";
 
 class App extends React.Component {
   constructor() {
@@ -95,7 +96,7 @@ class App extends React.Component {
         {
           first: "Kajetán",
           middle: "Laia",
-          last: "Saitou",
+          last: "Sorg",
           years: { MBA: 1997 }
         },
         {
@@ -153,91 +154,97 @@ class App extends React.Component {
     let x = [];
     let y = [];
     let z = [];
-    this.state.names.forEach((name, index) => {
-      const formattedName = (
-        <div key={index}>{`${name.first} ${name.middle} ${name.last}`}</div>
-      );
+
+    const sortedNames = sort(this.state.names).asc([
+      name => name.last,
+      name => name.first,
+      name => name.middle
+    ]);
+    sortedNames.forEach((name, index) => {
+      // this.state.names.forEach((name, index) => {
+      //   const formattedName = (
+      //     <div key={index}>{`${name.first} ${name.middle} ${name.last}`}</div>
+      //   );
       if (name.last[0] === "A") {
-        a.push(formattedName);
+        a.push(name);
       }
       if (name.last[0] === "B") {
-        b.push(formattedName);
+        b.push(name);
       }
       if (name.last[0] === "C") {
-        c.push(formattedName);
+        c.push(name);
       }
       if (name.last[0] === "D") {
-        d.push(formattedName);
+        d.push(name);
       }
       if (name.last[0] === "E") {
-        e.push(formattedName);
+        e.push(name);
       }
       if (name.last[0] === "F") {
-        f.push(formattedName);
+        f.push(name);
       }
       if (name.last[0] === "G") {
-        g.push(formattedName);
+        g.push(name);
       }
       if (name.last[0] === "H") {
-        h.push(formattedName);
+        h.push(name);
       }
       if (name.last[0] === "I") {
-        i.push(formattedName);
+        i.push(name);
       }
       if (name.last[0] === "J") {
-        j.push(formattedName);
+        j.push(name);
       }
       if (name.last[0] === "K") {
-        k.push(formattedName);
+        k.push(name);
       }
       if (name.last[0] === "L") {
-        l.push(formattedName);
+        l.push(name);
       }
       if (name.last[0] === "M") {
-        m.push(formattedName);
+        m.push(name);
       }
       if (name.last[0] === "N") {
-        n.push(formattedName);
+        n.push(name);
       }
       if (name.last[0] === "O") {
-        o.push(formattedName);
+        o.push(name);
       }
       if (name.last[0] === "P") {
-        p.push(formattedName);
+        p.push(name);
       }
       if (name.last[0] === "Q") {
-        q.push(formattedName);
+        q.push(name);
       }
       if (name.last[0] === "R") {
-        r.push(formattedName);
+        r.push(name);
       }
       if (name.last[0] === "S") {
-        s.push(formattedName);
+        s.push(name);
       }
       if (name.last[0] === "T") {
-        t.push(formattedName);
+        t.push(name);
       }
       if (name.last[0] === "U") {
-        u.push(formattedName);
+        u.push(name);
       }
       if (name.last[0] === "V") {
-        v.push(formattedName);
+        v.push(name);
       }
       if (name.last[0] === "W") {
-        w.push(formattedName);
+        w.push(name);
       }
       if (name.last[0] === "X") {
-        x.push(formattedName);
+        x.push(name);
       }
       if (name.last[0] === "Y") {
-        y.push(formattedName);
+        y.push(name);
       }
       if (name.last[0] === "Z") {
-        z.push(formattedName);
+        z.push(name);
       }
     });
     this.setState({
-      //TODO: alphabatize by last name
       a,
       b,
       c,
@@ -266,86 +273,138 @@ class App extends React.Component {
       z
     });
   }
-
+  //TODO: add years to names
   render() {
     return (
       <div>
         <Tabs defaultActiveKey="a-b">
           <Tab eventKey="a-b" title="A-B">
             <div>A</div>
-            {this.state.a}
+            {this.state.a.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>B</div>
-            {this.state.b}
+            {this.state.b.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="c-d" title="C-D">
             <div>C</div>
-            {this.state.c}
+            {this.state.c.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>D</div>
-            {this.state.d}
+            {this.state.d.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="e-f" title="E-F">
             <div>E</div>
-            {this.state.e}
+            {this.state.e.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>F</div>
-            {this.state.f}
+            {this.state.f.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="g-h" title="G-H">
             <div>G</div>
-            {this.state.g}
+            {this.state.g.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>H</div>
-            {this.state.h}
+            {this.state.h.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="i-j" title="I-J">
             <div>I</div>
-            {this.state.i}
+            {this.state.i.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>J</div>
-            {this.state.j}
+            {this.state.j.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="k-l" title="K-L">
             <div>K</div>
-            {this.state.k}
+            {this.state.k.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>L</div>
-            {this.state.l}
+            {this.state.l.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="m-n" title="M-N">
             <div>M</div>
-            {this.state.m}
+            {this.state.m.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>N</div>
-            {this.state.n}
+            {this.state.n.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="o-p" title="O-P">
             <div>O</div>
-            {this.state.o}
+            {this.state.o.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>P</div>
-            {this.state.p}
+            {this.state.p.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="q-r" title="Q-R">
             <div>Q</div>
-            {this.state.q}
+            {this.state.q.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>R</div>
-            {this.state.r}
+            {this.state.r.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="s-t" title="S-T">
             <div>S</div>
-            {this.state.s}
+            {this.state.s.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>T</div>
-            {this.state.t}
+            {this.state.t.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="u-v" title="U-V">
             <div>U</div>
-            {this.state.u}
+            {this.state.u.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>V</div>
-            {this.state.v}
+            {this.state.v.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
           <Tab eventKey="w-z" title="W-Z">
             <div>W</div>
-            {this.state.w}
+            {this.state.w.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>X</div>
-            {this.state.x}
+            {this.state.x.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>Y</div>
-            {this.state.y}
+            {this.state.y.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
             <div>Z</div>
-            {this.state.z}
+            {this.state.z.map((name, index) => {
+              return <div key={index}>{`${name.first} ${name.last}`}</div>;
+            })}
           </Tab>
         </Tabs>
       </div>
