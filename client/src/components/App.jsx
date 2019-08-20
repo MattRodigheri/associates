6c6ceeb1ed188c4fbe5a881dba27b90e040c7074
MattRodigheri <mrodigheri@gmail.com>
2019-08-19 20:40:15 -0700

    getting all data to tabs component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,49 +1,17 @@
 import React from "react";
-import * as contentful from "contentful";
 import axios from "axios";
 import keys from "../../../keys.js";
 import Counter from "./Counter.jsx";
 import Search from "./Search.jsx";
 import NameTabs from "./NameTabs.jsx";
 import "../../styles/app.css";
-import { totalmem } from "os";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {};
-
-    this.client = contentful.createClient({
-      space: keys.space,
-      accessToken: keys.accessToken
-    });
-
-    // this.fetchAssociate = this.fetchAssociates.bind(this);
-    // this.setAssociates = this.setAssociates.bind(this);
   }
-
-  // componentDidMount() {
-  //   this.fetchAssociates().then(this.setAssociates);
-  // }
-
-  // fetchAssociates() {
-  //   // for (const i = 0; i < t)
-  //   return this.client.getEntries({
-  //     limit: 1000
-  //   });
-  // }
-
-  // setAssociates(response) {
-  //   let names = [];
-  //   console.log(response.items);
-  //   response.items.forEach(person => {
-  //     names.push(person);
-  //   });
-  //   this.setState({
-  //     names
-  //   });
-  // }
 
   componentDidMount() {
     let names = [];
@@ -59,7 +27,8 @@ class App extends React.Component {
         });
 
         this.setState({
-          names
+          names,
+          total: response.data.total
         });
 
         const loops = Math.ceil(response.data.total / 1000);
@@ -75,53 +44,17 @@ class App extends React.Component {
               }&limit=1000&skip=${skip}`
             )
             .then(response => {
-              const totalNames = this.state.names.concat(response.data.items);
+              let newNames = [];
               response.data.items.forEach(person => {
-                names.push(person.fields);
+                newNames.push(person.fields);
               });
+              let totalNames = this.state.names.concat(newNames);
               this.setState({
                 names: totalNames
               });
             });
         }
       })
-
-      // .then(
-      //   axios
-      //     .get(
-      //       `https://cdn.contentful.com/spaces/${
-      //         keys.space
-      //       }/entries?access_token=${keys.accessToken}&limit=1000&skip=1000`
-      //     )
-      //     .then(response => {
-      //       // let names = [];
-      //       response.data.items.forEach(person => {
-      //         names.push(person.fields);
-      //       });
-      //       // this.setState({
-      //       //   names
-      //       // });
-      //       // console.log(this.state.names);
-      //     })
-      // )
-      // .then(
-      //   axios
-      //     .get(
-      //       `https://cdn.contentful.com/spaces/${
-      //         keys.space
-      //       }/entries?access_token=${keys.accessToken}&limit=1000&skip=2000`
-      //     )
-      //     .then(response => {
-      //       // let names = [];
-      //       response.data.items.forEach(person => {
-      //         names.push(person.fields);
-      //       });
-      //       this.setState({
-      //         names
-      //       });
-      //       console.log(this.state.names);
-      //     })
-      // )
       .catch(error => {
         console.log(error);
       });
@@ -137,7 +70,7 @@ class App extends React.Component {
           />
           <Search className="search" names={this.state.names} />
         </header>
-        {this.state && this.state.names && (
+        {this.state.names && this.state.names.length === this.state.total && (
           <NameTabs names={this.state.names} />
         )}
       </main>
